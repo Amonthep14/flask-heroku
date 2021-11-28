0eacb04769aceb67cbf4b760da142c959143139a
Amonthep14 <94913099+Amonthep14@users.noreply.github.com>
2021-11-28 22:13:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok" 
+    return "Hello นาย อมรเทพ สืบสอน เลขที่6 ม.4/9" 
 
 @app.route('/api', methods=['GET'])
 def get_api():
